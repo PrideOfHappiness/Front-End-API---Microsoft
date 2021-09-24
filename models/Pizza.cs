5835dea8608624b9240022ba633d060b49e7299f
Renaldi <soeryadi.renaldi@gmail.com>
2021-09-24 19:16:54 +0700

    Contoh API Back End Contoso Pizza

---
+++ models/Pizza.cs
@@ -0,0 +1,7 @@
+namespace ContossoPizza.models{
+    public class Pizza{
+        public int Id { get;set; }
+        public string Name { get;set;}
+        public bool IsGlutenFree { get; set;}
+    }
+}
